0d32887e9a29117fc269bdbf53ffa07795cc10c2
lrqnew <1406107400@qq.com>
2019-07-18 09:00:29 +0800

    完成用户中心布局

--- src/components/home/UserCenter.vue
+++ src/components/home/UserCenter.vue
@@ -5,28 +5,47 @@
       <span class="uemai">1406107400@qq.com</span>
       <Icon class="userSet" type="ios-cog-outline" size="22" />
     </h3>
-    <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
-      <div>
+    <Content :style="{padding: '24px', minHeight: '280px'}">
+      <!-- 内容统计面板 -->
+      <div class="total">
         <Row type="flex" justify="start">
           <Col span="8">
-            <Card style="width:320px">
-              <div style="text-align:center">
-                <h3>A high quality UI Toolkit based on Vue.js</h3>
-              </div>
+            <Card>
+              <div style="text-align:center">A high quality UI Toolkit based on Vue.js</div>
             </Card>
           </Col>
           <Col span="8">
-            <Card style="width:320px">
-              <div style="text-align:center">
-                <h3>A high quality UI Toolkit based on Vue.js</h3>
-              </div>
+            <Card>
+              <div style="text-align:center">A high quality UI Toolkit based on Vue.js</div>
             </Card>
           </Col>
           <Col span="8">
-            <Card style="width:320px">
-              <div style="text-align:center">
-                <h3>A high quality UI Toolkit based on Vue.js</h3>
-              </div>
+            <Card>
+              <div style="text-align:center">A high quality UI Toolkit based on Vue.js</div>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+      <!-- 内容底部 -->
+      <div class="content_bot">
+        <Row>
+          <Col span="16" >
+            <Card style="width:90%;height:400px;" dis-hover :bordered="false">
+              <p slot="title">
+                <Icon type="md-create" />日记概览
+              </p>
+              <a href="#" slot="extra">
+                <Icon type="md-more" />
+              </a>
+             
+            </Card>
+          </Col>
+          <Col span="8">
+           <Card style="width:90%;height:400px;" dis-hover :bordered="false">
+              <p slot="title">
+                <Icon type="ios-cloud" />今日天气
+              </p>
+            
             </Card>
           </Col>
         </Row>
@@ -49,6 +68,28 @@ h3 > .uemai {
 }
 .userSet {
   margin-left: 10px;
+}
+.total {
+  width: 90%;
+  color: #fff;
+}
+.total .ivu-row-flex .ivu-col .ivu-card {
+  width: 400px;
+  height: 100px;
+  line-height: 100px;
+}
+.total .ivu-row-flex .ivu-col:nth-child(1) .ivu-card {
+  background: #2db7f5;
+}
+.total .ivu-row-flex .ivu-col:nth-child(2) .ivu-card {
+  background: #19be6b;
+}
+.total .ivu-row-flex .ivu-col:nth-child(3) .ivu-card {
+  background: #ed4014;
+}
+.content_bot{
+    margin-top: 20px;
+    width: 90%;
 }
 </style>
 
